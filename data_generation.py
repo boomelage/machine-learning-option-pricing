4262668feb163baf182ad40a926a10f1beaab266
radusbriciu <radusbriciu@proton.me>
2024-09-05 18:35:23 +0100

    further development. need to find a method for generating the IVOLs for strikes and maturities

--- data_generation.py
+++ data_generation.py
@@ -21,11 +21,12 @@ def generate_data_subset(S):
     
     lower_moneyness = 0.2
     upper_moneyness = 1.5
-    nstrikes = 8
-    K = np.linspace(S * lower_moneyness, S * upper_moneyness, nstrikes)
+    n_strikes = 100
+    K = np.linspace(S * lower_moneyness, S * upper_moneyness, n_strikes)
     
-    T = np.arange(3/12, 2.01, 1/12)
-    n_maturities = len(T)
+    n_maturities = 100
+    # T = np.arange(3/12, 2.01, 1/12)
+    T = np.linspace(3/12, 2.01, n_maturities)
     
     def generate_features():
         features = pd.DataFrame(
@@ -40,11 +41,24 @@ def generate_data_subset(S):
     
     features = generate_features()
     
-    min_vol = 0.28
-    max_vol = 0.4
-    n_vols = n_maturities * nstrikes
-    sigma = np.linspace(min_vol, max_vol, n_vols)
-    features['volatility'] = sigma
+    # min_vol = 0.28
+    # max_vol = 0.4
+    # n_vols = n_maturities * n_strikes
+    # sigma = np.linspace(min_vol, max_vol, n_vols)
+    # features['volatility'] = sigma
+    # Define the number of lists and the range size
+    
+
+    lower_vol = 0.28
+    upper_vol = 0.40
+    n_vols = n_strikes
+    
+    data = [np.linspace(lower_vol, upper_vol, n_vols) for _ in range(n_vols)]
+    data = [arr.tolist() for arr in data]
+    
+    flattened_data = [item for sublist in data for item in sublist]
+    features['volatility'] = flattened_data
+    
     
     r = 0.05
     features['risk_free_rate'] = r
@@ -61,17 +75,16 @@ def generate_data_subset(S):
         lambda row: row['calculation_date'] + ql.Period(
             int(math.floor(row['years_to_maturity'] * 365)), ql.Days), axis=1)
     
-    data = vanilla_params
     
     # rates = np.ones(len(T))*r
     
-    expiration_dates = data['maturity_date'].unique()
+    expiration_dates = vanilla_params['maturity_date'].unique()
     
-    strikes = data['strike_price'].unique()
+    strikes = vanilla_params['strike_price'].unique()
     
     implied_vols = ql.Matrix(len(strikes), len(expiration_dates))
     
-    calibrated_features = calibrate_heston(vanilla_params, dividend_rate, r, implied_vols)
+    calibrated_features = calibrate_heston(vanilla_params, dividend_rate, r, implied_vols, data)
     
     prices = heston_price_vanillas(calibrated_features)
     
@@ -87,6 +100,7 @@ def generate_dataset(spots):
         data_subsets.append(subset)
     dataset =pd.concat(data_subsets, ignore_index=True)
     return dataset
+
 
 
         
