436a690a52bf36d212ad0d7cf65b7b360f18a37a
radusbriciu <radusbriciu@proton.me>
2024-09-05 01:12:02 +0100

    calibration still doesn't work. simply returns initial guess.

--- data_generation3.py
+++ data_generation3.py
@@ -12,18 +12,21 @@ from itertools import product
 import QuantLib as ql
 import math
 from pricing import BS_price_vanillas
-S = 100
+S = 220
 spots = np.ones(1) * S
 
 lower_moneyness = 0.2
 upper_moneyness = 1.5
-nstrikes = 5
+nstrikes = 100
 K = np.linspace(S * lower_moneyness, S * upper_moneyness, nstrikes)
 
-smallest_mat = 1/252
-biggest_mat = 1.5
-n_maturities = 2
-T = np.linspace(smallest_mat, biggest_mat, n_maturities)
+# smallest_mat = 1/365
+# biggest_mat = 1.5
+# n_maturities = 20
+# T = np.linspace(smallest_mat, biggest_mat, n_maturities)
+
+T = np.arange(3/12, 2.01, 1/12)
+n_maturities = len(T)
 
 def generate_features():
     features = pd.DataFrame(
@@ -46,18 +49,16 @@ n_vols = n_maturities * nstrikes
 sigma = np.linspace(min_vol, max_vol, n_vols)
 
 features['volatility'] = sigma
-features['risk_free_rate'] = 0.01
+r = 0.01
+features['risk_free_rate'] = r
 features['w'] = 1
 
 # r = np.linspace(0.005,0.05,n_maturities)
 # for j in range (0,len(sigma)):
 #     if features['years_to_maturity'][j] == T[i]:
 #        features['risk_free_rate'][j] = r[i]
-features['calculation_date'] = ql.Date.todaysDate()
-features['maturity_date'] = features.apply(
-    lambda row: row['calculation_date'] + ql.Period(
-        int(math.floor(row['years_to_maturity'] * 365)), ql.Days), axis=1)
-∂
+
+
 vanilla_prices = BS_price_vanillas(features)
 
 vanilla_prices['dividend_rate'] = 0
@@ -67,5 +68,23 @@ vanilla_prices['theta'] = 0.04
 vanilla_prices['sigma'] = 0.2
 vanilla_prices['rho'] = -0.5
 
-print(vanilla_prices)
+vanilla_prices['calculation_date'] = ql.Date.todaysDate()
+vanilla_prices['maturity_date'] = vanilla_prices.apply(
+    lambda row: row['calculation_date'] + ql.Period(
+        int(math.floor(row['years_to_maturity'] * 365)), ql.Days), axis=1)
 
+
+row = vanilla_prices.iloc[0,:]
+
+row['calculation_date']
+
+data = vanilla_prices
+
+rates = np.ones(len(T))*r
+
+from borrowed_heston import QuantlibCalibration
+
+
+params = QuantlibCalibration.fit_model(data)
+
+print(f'\n{params}')
