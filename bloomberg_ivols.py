f79b982c63a2e97b023fdd05370edc1e53bc70ca
radusbriciu <radusbriciu@proton.me>
2024-09-07 21:50:55 +0100

    working on parsing ivol table from bloomberg bidask ivols

--- bloomberg_ivols.py
+++ bloomberg_ivols.py
@@ -11,6 +11,7 @@ import pandas as pd
 from itertools import product
 import QuantLib as ql
 import math
+from heston_calibration import calibrate_heston
 
 risk_free_rate = 0.00
 dividend_rate = 0.00
@@ -18,13 +19,14 @@ dividend_rate = 0.00
 bbivols = pd.read_excel(r'22000 AAPL.xlsx')
 
 bbivols.columns = bbivols.iloc[1]
-
 bbivols = bbivols.drop([0,1])
 bbivols = bbivols.reset_index(drop=True)
+bbivols = bbivols.sort_values(by = 'Strike')
 bbivols = bbivols.dropna()
 
 maturities = bbivols.filter(like='DyEx').iloc[0].unique()
-strikes = bbivols.filter(like='Strike').to_numpy()
+
+strikes = bbivols.filter(like='Strike').to_numpy().astype(float).flatten()
 
 n_maturities = len(maturities)
 n_strikes = len(strikes)
@@ -34,13 +36,14 @@ bbivs = bbiv_df.to_numpy()
 
 n_bbivs = bbivs.shape[0]
 
+
+
 ivol_table = np.empty(n_maturities, dtype=object)
 for i in np.arange(1,n_bbivs,2):
     ivol_table[int((i+1)/2)-1] = (bbivs[i]+bbivs[i+1])/2
 
 
-
-S = [int(np.median(strikes))]
+S = [np.median(strikes)]
 K = strikes
 T = maturities
 def generate_features():
@@ -63,40 +66,44 @@ option_data['maturity_date'] = option_data.apply(
     lambda row: row['calculation_date'] + ql.Period(
         int(math.floor(row['years_to_maturity'] * 365)), ql.Days), axis=1)
 
-prepare_calibration(self, ivol_table, option_data, dividend_rate, risk_free_rate):
-    day_count = ql.Actual365Fixed()
-    calendar = ql.UnitedStates(m=1)
-    
-    calculation_date = option_data['calculation_date'][0]
-    spot = option_data['spot_price'][0]
-    ql.Settings.instance().evaluationDate = calculation_date
-    
-    
-    dividend_yield = ql.QuoteHandle(ql.SimpleQuote(dividend_rate))
-    dividend_rate = dividend_yield
-    flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, risk_free_rate, day_count))
-    dividend_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, dividend_rate, day_count))
-    
-    
-    expiration_dates = option_data['maturity_date'].unique()
-    
-    strikes = option_data['strike_price'].unique()
 
-    implied_vols = ql.Matrix(len(strikes), len(expiration_dates))
-    
-    for i in range(implied_vols.rows()):
-        for j in range(implied_vols.columns()):
-            implied_vols[i][j] = ivol_table[j][i]
-    black_var_surface = ql.BlackVarianceSurface(
-        calculation_date, calendar,
-        expiration_dates, strikes,
-        implied_vols, day_count)
-    
-    implied_vols_matrix = ql.Matrix(len(strikes), len(expiration_dates))
-    for i in range(implied_vols_matrix.rows()):
-        for j in range(implied_vols_matrix.columns()):
-            implied_vols_matrix[i][j] = ivol_table[j][i]  
-            
-    return option_data,flat_ts,dividend_ts,spot,expiration_dates, \
-        black_var_surface,strikes,day_count,calculation_date, calendar, \
-            implied_vols_matrix
+
+spot = ql.SimpleQuote(float(S[0]))
+
+
+calculation_date = ql.Date.todaysDate()
+expiration_dates = []
+for maturity in maturities:
+    expiration_date = calculation_date + ql.Period(int(maturity), ql.Days)  # Adjust conversion as needed
+    expiration_dates.append(expiration_date)
+day_count = ql.Actual365Fixed()
+day_count = ql.Actual365Fixed()
+calendar = ql.UnitedStates(m=1)
+ql.Settings.instance().evaluationDate = calculation_date
+dividend_yield = ql.QuoteHandle(ql.SimpleQuote(dividend_rate))
+dividend_rate = dividend_yield
+flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, risk_free_rate, day_count))
+dividend_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, dividend_rate, day_count))
+
+
+implied_vols_matrix = ql.Matrix(len(K),len(expiration_dates),0)
+                                 ##### ^ NEEDS CORRECTION, TOO HUNGRY
+implied_vols_matrix[10][0] = ivol_table[4][0]
+implied_vols_matrix[10][4] = ivol_table[4][4]
+
+for j in range(0,len(K)-1):
+    for i in range(0,len(expiration_dates)):
+        implied_vols_matrix[j][i] = ivol_table[i][j]
+
+print(implied_vols_matrix)
+
+black_var_surface = ql.BlackVarianceSurface(
+    calculation_date, calendar,
+    expiration_dates, K,
+    implied_vols_matrix, day_count)
+
+heston_params = calibrate_heston(option_data,flat_ts,dividend_ts,spot,expiration_dates,
+    black_var_surface,strikes,day_count,calculation_date, calendar,
+        dividend_rate, implied_vols_matrix)
+
+heston_params
