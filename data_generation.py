f24255cd2683734cae02828a589a8f10e7cd8eb3
radusbriciu <radusbriciu@proton.me>
2024-09-07 01:07:26 +0100

    further testing

--- data_generation.py
+++ data_generation.py
@@ -17,7 +17,7 @@ from generate_ivols import generate_ivol_table
 dividend_rate = 0.0
 r = 0.05
 
-pricing_range = 1.03
+pricing_range = 1.1
 
 ticker = 'AAPL'
 current_spot = 220.00
@@ -30,12 +30,14 @@ spots_subdivision = 3
 
 spotmin = int(current_spot/pricing_range)
 spotmax = int(current_spot*pricing_range)
-nspots = 3 # int(spots_subdivision*(spotmax-spotmin))
+# nspots = int(spots_subdivision*(spotmax-spotmin))
+nspots = 3
 
 lower_moneyness = tl_strike/current_spot
 upper_moneyness = current_spot/tl_strike
-n_strikes = 5# int((spots_subdivision+2)*(current_spot*upper_moneyness-\
-                    #current_spot*lower_moneyness))
+# n_strikes = int((spots_subdivision+2)*(current_spot*upper_moneyness-\
+#                                        current_spot*lower_moneyness))
+n_strikes = 15
 
 # =============================================================================
 tl_ivol = tl_ivol_q/100
