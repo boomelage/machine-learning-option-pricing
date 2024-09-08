154f1fc21bf67293f70efc9d24ce715ea5ddb1b0
boomelage <163606270+boomelage@users.noreply.github.com>
2024-09-08 16:49:13 +0100

    clean

--- callsputs.py
+++ callsputs.py
@@ -3,31 +3,28 @@
 """
 Created on Sun Sep  8 14:59:04 2024
 
+A function that collects option data given there is an even number of columns
+equally split between for calls and puts repsectively
+
 """
 import os
 pwd = str(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(pwd)
-import numpy as np
 import pandas as pd
-from itertools import product
+from datapwd import dirdata
 import QuantLib as ql
-import math
 import warnings
 warnings.simplefilter(action='ignore')
+data_files = dirdata()
+import numpy as np
 
-# Adjust pandas settings to display all rows and columns
+
 pd.set_option('display.max_rows', None)  # Display all rows
 pd.set_option('display.max_columns', None)  # Display all columns
 
-# Optionally, reset the settings to default
 # pd.reset_option('display.max_rows')
 # pd.reset_option('display.max_columns')
 
-# from heston_calibration import calibrate_heston
-# from pricing import heston_price_vanillas, noisyfier
-
-from datapwd import dirdata
-data_files = dirdata()
 
 def clean_data():
     calls = pd.DataFrame()
@@ -47,18 +44,41 @@ def clean_data():
         puts = pd.concat([puts, octoputs], ignore_index=True)
         calls = calls.sort_values(by = 'Strike')
         puts = puts.sort_values(by = 'Strike')
+    calls['IVM'] = calls['IVM']/100
+    puts['IVM'] = puts['IVM']/100
     return calls, puts
 
+def group_by_maturity(df):
+    grouped = df.groupby('DyEx')
+    group_arrays = []
+    for _, group in grouped:
+        group_array = group.values
+        group_arrays.append(group_array)
+    final_array = np.array(group_arrays, dtype=object)
+    return final_array
+
+
+
+# full option data
 calls, puts = clean_data()
 
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+# ivol table generation
+
+callvols = calls.copy().drop(columns = ['w','Rate'])
+groupedmat = group_by_maturity(callvols)
+n_maturities = int(len(groupedmat))
+n_strikes = int(len(groupedmat[0]))
+
+implied_vols_matrix = ql.Matrix(n_strikes,n_maturities,float(0))
+
+for i in range(n_maturities):
+    for j in range(n_strikes):
+        implied_vols_matrix[j][i] = groupedmat[i][j][1]
+
+
+
+
+print(implied_vols_matrix)
+
 
