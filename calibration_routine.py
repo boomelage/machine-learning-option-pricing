54a71de61248fb83be0423fb63c7b40238c86f25
radusbriciu <radusbriciu@proton.me>
2024-09-09 03:27:42 +0100

    further testing

--- calibration_routine.py
+++ calibration_routine.py
@@ -8,8 +8,7 @@ calibration_routine
 import os
 pwd = str(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(pwd)
-import pandas as pd
-from datapwd import dirdata
+from data_query import dirdata
 import QuantLib as ql
 import warnings
 warnings.simplefilter(action='ignore')
@@ -32,28 +31,41 @@ dividend_ts = ql.YieldTermStructureHandle(ql.FlatForward(
     calculation_date, dividend_rate, day_count))
 
 
-from collect_ivols import n_maturities, n_strikes, expiration_dates, \
-    implied_vol_matrix, strikes, maturities, ivols, S
-                                                                  
-Ks = strikes
-minK = min(Ks)
-maxK = max(Ks)
-lower_moneyness = minK/S
-upper_moneyness = maxK/S
-shortest_maturity = min(maturities)
-longest_maturity = max(maturities)
 
-K = strikes
-T = maturities
-
-nTs = (longest_maturity - shortest_maturity)*3
-nKs = (maxK-minK)*5
-T = np.linspace(shortest_maturity, longest_maturity,nTs)
-K = np.linspace(minK,maxK,nKs)
+     
 
                                                                   #------------
                                                                   # market data
-# option_data = ivols.copy()
+
+
+data_files = dirdata()
+
+
+f
+calls, strikes, maturities, ivols = fetch_bbdata(data_files, calculation_date)
+n_maturities, n_strikes, S, expiration_dates, implied_vol_matrix = \
+    make_ivol_matrix(strikes,maturities,ivols,calculation_date)
+
+
+
+# Ks = strikes
+# minK = min(Ks)
+# maxK = max(Ks)
+# lower_moneyness = minK/S
+# upper_moneyness = maxK/S
+# shortest_maturity = min(maturities)
+# longest_maturity = max(maturities)
+
+# K = strikes
+# T = maturities
+
+# nTs = (longest_maturity - shortest_maturity)*3
+# nKs = (maxK-minK)*5
+# data_T = np.linspace(shortest_maturity, longest_maturity,nTs)
+# data_K = np.linspace(minK,maxK,nKs)
+
+
+# option_data = calls.copy()
 # option_data.columns = ['strike_price',
 #                         'volatility',
 #                         'days_to_matuirty',
@@ -63,35 +75,31 @@ K = np.linspace(minK,maxK,nKs)
 # option_data['calculation_date'] = calculation_date
 # option_data
 
-                                                               #---------------
-                                                               # generated data
-from data_generation import data_generation
-dg = data_generation(lower_moneyness=lower_moneyness, upper_moneyness=upper_moneyness,
-              T=T, n_maturities = n_maturities, n_strikes = n_strikes,
-              risk_free_rate = risk_free_rate, dividend_rate = dividend_rate)
 
-option_data = dg.generate_data_subset(S)
-option_data
+
+
 
 # =============================================================================
                                                      # calibrating Heston model
 
-black_var_surface = ql.BlackVarianceSurface(
-    calculation_date, calendar,
-    expiration_dates, K,
-    implied_vol_matrix, day_count)
+# black_var_surface = ql.BlackVarianceSurface(
+#     calculation_date, calendar,
+#     expiration_dates, K,
+#     implied_vol_matrix, day_count)
 
-from heston_calibration import calibrate_heston
-heston_params = calibrate_heston(
-    option_data,flat_ts,dividend_ts, S, expiration_dates, 
-    black_var_surface, K, day_count,calculation_date, calendar, 
-    dividend_rate, implied_vol_matrix)
+# from heston_calibration import calibrate_heston
+# heston_params = calibrate_heston(
+#     option_data,flat_ts,dividend_ts, S, expiration_dates, 
+#     black_var_surface, K, day_count,calculation_date, calendar, 
+#     dividend_rate, implied_vol_matrix)
 
-                                                                      #--------
-                                                                      # pricing
-from pricing import heston_price_vanillas, noisyfier
-heston_vanillas = heston_price_vanillas(heston_params)
-dataset = noisyfier(heston_vanillas)
+#                                                                       #--------
+#                                                                       # pricing
+# from pricing import heston_price_vanillas, noisyfier
+# heston_vanillas = heston_price_vanillas(heston_params)
+# dataset = noisyfier(heston_vanillas)
+
+# dataset
 
 
 
@@ -101,11 +109,14 @@ dataset = noisyfier(heston_vanillas)
 # import matplotlib.pyplot as plt
 # from matplotlib import cm
 
+
 # plt.rcParams['figure.figsize']=(6,4)
 # plt.style.use("dark_background")
 
 # expiry = 1/365
-# target_maturity_ivols = ivols.iloc[0,:]
+# target_maturity_ivols = ivols.iloc[:,0].to_numpy()
+
+
 # implied_vols = [black_var_surface.blackVol(expiry, k)
 #                 for k in K]
 
@@ -117,12 +128,9 @@ dataset = noisyfier(heston_vanillas)
 # legend = ax.legend(loc="upper right")
 # fig.show()
 
-# plottmin = min(maturities)
-# plottmax = max(maturities)
-# plot_maturities = np.linspace(plottmin, plottmax, len(maturities))
 
+# plot_maturities = maturities/365.25
 # X, Y = np.meshgrid(K, plot_maturities)
-
 # Z = np.array([[black_var_surface.blackVol(y, x) for x in K] for y in plot_maturities])
 
 # fig = plt.figure()
--- data_generation.py
+++ data_generation.py
@@ -10,110 +10,28 @@ from itertools import product
 import QuantLib as ql
 import math
 
-class data_generation():
-    def __init__(self, lower_moneyness=None, upper_moneyness=None,
-                 T=None, n_maturities = None, n_strikes = None,
-                 risk_free_rate = None, dividend_rate = None):
-        self.lower_moneyness = lower_moneyness
-        self.upper_moneyness = upper_moneyness
-        self.T = T
-        self.n_maturities = n_maturities
-        self.n_strikes = n_strikes
-        self.risk_free_rate = risk_free_rate
-        self.dividend_rate = dividend_rate
-
-    def generate_data_subset(self,S):
-        subset_spot = np.ones(1) * S
-        K = np.linspace(S * self.lower_moneyness, S * self.upper_moneyness, 
-                        self.n_strikes)
-        def generate_features():
-            features = pd.DataFrame(
-                product(subset_spot, K, self.T),
-                columns=[
-                    "spot_price", 
-                    "strike_price", 
-                    "days_to_maturity"
-                         ])
-            return features
-        features = generate_features()
-        features['risk_free_rate'] = self.risk_free_rate
-        features['dividend_rate'] = self.dividend_rate
-        features['w'] = 1
-        option_data = features
-        option_data['calculation_date'] = ql.Date.todaysDate()
-        option_data['maturity_date'] = option_data.apply(
-            lambda row: row['calculation_date'] + ql.Period(
-                int(math.floor(row['days_to_maturity'])), ql.Days), axis=1)
-        return option_data
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# =============================================================================
-# =============================================================================
-# =============================================================================
-# =============================================================================
-# =============================================================================
-# # # # #     def prepare_calibration(self, ivol_table, option_data, dividend_rate, risk_free_rate):
-# # # # #         day_count = ql.Actual365Fixed()
-# # # # #         calendar = ql.UnitedStates(m=1)
-# # # # #         
-# # # # #         calculation_date = option_data['calculation_date'][0]
-# # # # #         spot = option_data['spot_price'][0]
-# # # # #         ql.Settings.instance().evaluationDate = calculation_date
-# # # # #         
-# # # # #         
-# # # # #         dividend_yield = ql.QuoteHandle(ql.SimpleQuote(dividend_rate))
-# # # # #         dividend_rate = dividend_yield
-# # # # #         flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, risk_free_rate, day_count))
-# # # # #         dividend_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, dividend_rate, day_count))
-# # # # #         
-# # # # #         expiration_dates = option_data['maturity_date'].unique()
-# # # # #         
-# # # # #         strikes = option_data['strike_price'].unique()
-# # # # #     
-# # # # #         implied_vols = ql.Matrix(len(strikes), len(expiration_dates))
-# # # # #         
-# # # # #         for i in range(implied_vols.rows()):
-# # # # #             for j in range(implied_vols.columns()):
-# # # # #                 implied_vols[i][j] = ivol_table[j][i]
-# # # # #         black_var_surface = ql.BlackVarianceSurface(
-# # # # #             calculation_date, calendar,
-# # # # #             expiration_dates, strikes,
-# # # # #             implied_vols, day_count)
-# # # # #         
-# # # # #         implied_vols_matrix = ql.Matrix(len(strikes), len(expiration_dates))
-# # # # #         for i in range(implied_vols_matrix.rows()):
-# # # # #             for j in range(implied_vols_matrix.columns()):
-# # # # #                 implied_vols_matrix[i][j] = ivol_table[j][i]  
-# # # # #                 
-# # # # #         return option_data,flat_ts,dividend_ts,spot,expiration_dates, \
-# # # # #             black_var_surface,strikes,day_count,calculation_date, calendar, \
-# # # # #                 implied_vols_matrix
-# =============================================================================
-# =============================================================================
-# =============================================================================
-# =============================================================================
-# 
-# =============================================================================
-
+def generate_dataset(
+        S, lower_moneyness, upper_moneyness, nKs, risk_free_rate, data_T, 
+        dividend_rate):
+    subset_spot = np.ones(1) * S
+    K = np.linspace(S * lower_moneyness, S * upper_moneyness, 
+                    nKs)
+    def generate_features():
+        features = pd.DataFrame(
+            product(subset_spot, K, data_T),
+            columns=[
+                "spot_price", 
+                "strike_price", 
+                "days_to_maturity"
+                     ])
+        return features
+    features = generate_features()
+    features['risk_free_rate'] = risk_free_rate
+    features['dividend_rate'] = dividend_rate
+    features['w'] = 1
+    option_data = features
+    option_data['calculation_date'] = ql.Date.todaysDate()
+    option_data['maturity_date'] = option_data.apply(
+        lambda row: row['calculation_date'] + ql.Period(
+            int(math.floor(row['days_to_maturity'])), ql.Days), axis=1)
+    return option_data
