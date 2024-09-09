c6fcb592d309f60604f9923ae773513cbfac9adb
boomelage <163606270+boomelage@users.noreply.github.com>
2024-09-09 20:47:38 +0100

    need to iterate through calibration routine for all underlyings, not together

--- collect_market_data.py
+++ collect_market_data.py
@@ -9,48 +9,52 @@ import os
 pwd = str(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(pwd)
 import pandas as pd
+import numpy as np                                                     
+pd.set_option('future.no_silent_downcasting', True)
+import QuantLib as ql
+from pricing import heston_price_vanillas, noisyfier
 from data_query import dirdata
 
-# pd.set_option('display.max_rows', None)  # To display all rows
-# pd.set_option('display.max_columns', None)  # To display all columns
-pd.reset_option('display.max_rows')  # To display all rows
-pd.reset_option('display.max_columns')  # To display all columns
 
-# =============================================================================
-                                                                     # cleaning 
 
 def clean_data(file):
     df = pd.read_excel(file)
     df.columns = df.loc[0]
     df = df.iloc[1:,:]
-    
-    df['DvYd'] =  df['DvYd'].fillna(0)
+    df['DvYd'] = df['DvYd'].fillna(0).infer_objects(copy=False)
     df = df.dropna().copy()
     df = df.drop_duplicates()
     df = df.reset_index(drop=True)
     splitter = int(df.shape[1]/2)
     dfcalls_subset = df.iloc[:,:splitter]
-    
+    subset_spot_price = np.median(np.array(dfcalls_subset['Strike'].unique()))
+    dfcalls_subset['spot_price'] = subset_spot_price
+    dfcalls_subset.rename(columns={'Strike': 'strike_price'}, inplace=True)
+    dfcalls_subset.rename(columns={'IVM': 'volatility'}, inplace=True)
+    dfcalls_subset.rename(columns={'DyEx': 'days_to_maturity'}, inplace=True)
+    dfcalls_subset.rename(columns={'Rate': 'risk_free_rate'}, inplace=True)
+    dfcalls_subset.rename(columns={'DvYd': 'dividend_rate'}, inplace=True)
+    dfcalls_subset['calculation_date'] = ql.Date.todaysDate()
+    def calculate_maturity_date(row, calc_date):
+        return calc_date + ql.Period(int(row['days_to_maturity']), ql.Days)
+    dfcalls_subset['maturity_date'] = dfcalls_subset.apply(
+        calculate_maturity_date,calc_date=ql.Date.todaysDate(),axis=1)
+    dfcalls_subset['years_to_maturity'] = dfcalls_subset['days_to_maturity']/365
+
     return dfcalls_subset
 
-
-# =============================================================================
-                                                           # concatinating data
 
 def concat_data(data_files):
     dataset = pd.DataFrame()  
     for file in data_files:
-        dfcalls_subset = clean_data(file)
-        dataset = pd.concat([dataset, dfcalls_subset], ignore_index=True)
-        # dfcalls_subset['Strike'] = dfcalls_subset['Strike']
-        # dfcalls_subset['DyEx'] = dfcalls_subset['DyEx']
-        # dfcalls_subset['IVM'] = dfcalls_subset['IVM']/100
-        # dfcalls_subset['Rate'] = dfcalls_subset['Rate']/100
-        # dfcalls_subset['DvYd'] = dfcalls_subset['DvYd']/100
+        try:
+            dfcalls_subset = clean_data(file)
+            dataset = pd.concat([dataset, dfcalls_subset], ignore_index=True)
+        except Exception as e:
+            print(f'problem with {file}: {e}')
+
     return dataset
 
+dataset = concat_data(dirdata())
 
-subset = clean_data(dirdata()[0])
-subset
-
-
+dataset.loc[0]
