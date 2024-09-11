a04fe5170845f5e5e1f0f5b41a2f337b68a0b51b
radusbriciu <radusbriciu@proton.me>
2024-09-11 15:03:58 +0100

    multiple

--- settings.py
+++ settings.py
@@ -2,9 +2,6 @@
 """
 Created on Mon Sep  9 13:54:57 2024
 
-a set of functions that help convert python arrays and lists into various
-quantlib objects
-
 """
 import QuantLib as ql
 import numpy as np
@@ -16,18 +13,15 @@ class model_settings():
         self.dividend_rate = dividend_rate = 0.005
         self.risk_free_rate = risk_free_rate = 0.05
         self.calculation_date = calculation_date = ql.Date.todaysDate()
-        
         self.dday_count = day_count = ql.Actual365Fixed()
         self.calendar = ql.UnitedStates(m=1)
-        
-        
 
         self.flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(
             self.calculation_date, self.risk_free_rate, self.day_count))
         self.dividend_ts = ql.YieldTermStructureHandle(ql.FlatForward(
             self.calculation_date, self.dividend_rate, self.day_count))
-        
         self.dividend_rate = ql.QuoteHandle(ql.SimpleQuote(dividend_rate)) 
+        
         ql.Settings.instance().evaluationDate = calculation_date
     
     def make_ql_array(size,nparr):
