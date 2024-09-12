9e3deea78265a337386bb522d37f7187d48e6b94
radusbriciu <radusbriciu@proton.me>
2024-09-12 19:32:08 +0100

    major

--- routine_calibration_redesign.py
+++ routine_calibration_redesign.py
@@ -57,51 +57,55 @@ engine = ql.AnalyticHestonEngine(model)
 print(process)
 heston_helpers = []
 
-for current_index, date in enumerate(expiration_dates):
-    print(f"\nCurrently calibrating for maturity: {date}")
-    # black_var_surface.setInterpolation("bicubic")
-    for j, s in enumerate(Ks):
-       t = day_count.yearFraction(calculation_date, date)
-       sigma = black_var_surface.blackVol(t, s)  
-       helper = ql.HestonModelHelper(
-           ql.Period(int(t * 365), ql.Days),
-           calendar, S, s,
-           ql.QuoteHandle(ql.SimpleQuote(sigma)),
-           flat_ts, dividend_ts
-           )
-       helper.setPricingEngine(engine)
-       heston_helpers.append(helper)
-    lm = ql.LevenbergMarquardt(1e-8, 1e-8, 1e-8)
-    model.calibrate(heston_helpers, lm,
-                     ql.EndCriteria(500, 50, 1.0e-8,1.0e-8, 1.0e-8))
-    theta, kappa, sigma, rho, v0 = model.params()
-    
-    print (
-        "\ntheta = %f, kappa = %f, "
-        "sigma = %f, rho = %f, v0 = %f" % (theta, kappa, 
-                                           sigma, rho, v0))
-    avg = 0.0
-    time.sleep(0.005)
-    print ("%15s %15s %15s %20s" % (
-        "Strikes", "Market Value",
-          "Model Value", "Relative Error (%)"))
-    print ("="*70)
-    for i in range(min(len(heston_helpers), len(Ks))):
-        opt = heston_helpers[i]
-        err = (opt.modelValue() / opt.marketValue() - 1.0)
-        print(f"{Ks[i]:15.2f} {opt.marketValue():14.5f} "
-              f"{opt.modelValue():15.5f} {100.0 * err:20.7f}")
-        avg += abs(err)  # accumulate the absolute error
-    avg = avg*100.0/len(heston_helpers)
-    print("-"*70)
-    print("Total Average Abs Error (%%) : %5.3f" % (avg))
-    heston_params = {
-        'theta':theta, 
-        'kappa':kappa, 
-        'sigma':sigma, 
-        'rho':rho, 
-        'v0':v0
-        }
+
+calibration_data = None
+
+for row in calibration_data.index:
+   calibration_data = calibration_data.loc[row]
+   k = calibration_data.loc['strike_price']
+   t = day_count.yearFraction(
+       calculation_date, calibration_data['maturity_date'])
+   sigma = black_var_surface.blackVol(t, k)  
+   helper = ql.HestonModelHelper(
+       ql.Period(int(t * 365), ql.Days),
+       calendar, S, k,
+       ql.QuoteHandle(ql.SimpleQuote(sigma)),
+       flat_ts, dividend_ts
+       )
+   helper.setPricingEngine(engine)
+   heston_helpers.append(helper)
+lm = ql.LevenbergMarquardt(1e-8, 1e-8, 1e-8)
+model.calibrate(heston_helpers, lm,
+                 ql.EndCriteria(500, 50, 1.0e-8,1.0e-8, 1.0e-8))
+theta, kappa, sigma, rho, v0 = model.params()
+
+print (
+    "\ntheta = %f, kappa = %f, sigma = %f, rho = %f, v0 = %f" \
+        % \
+            (theta, kappa, sigma, rho, v0)
+    )
+avg = 0.0
+time.sleep(0.005)
+print ("%15s %15s %15s %20s" % (
+    "Strikes", "Market Value",
+      "Model Value", "Relative Error (%)"))
+print ("="*70)
+for i in range(min(len(heston_helpers), len(Ks))):
+    opt = heston_helpers[i]
+    err = (opt.modelValue() / opt.marketValue() - 1.0)
+    print(f"{Ks[i]:15.2f} {opt.marketValue():14.5f} "
+          f"{opt.modelValue():15.5f} {100.0 * err:20.7f}")
+    avg += abs(err)  # accumulate the absolute error
+avg = avg*100.0/len(heston_helpers)
+print("-"*70)
+print("Total Average Abs Error (%%) : %5.3f" % (avg))
+heston_params = {
+    'theta':theta, 
+    'kappa':kappa, 
+    'sigma':sigma, 
+    'rho':rho, 
+    'v0':v0
+    }
 
 print('\nHeston model parameters:')
 for key, value in heston_params.items():
