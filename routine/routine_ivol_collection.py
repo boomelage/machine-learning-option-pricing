e869ea96cf541ad61e7a257e526d90082839d963
radusbriciu <radusbriciu@proton.me>
2024-09-14 13:22:03 +0100

    clean

--- routine/routine_ivol_collection.py
+++ routine/routine_ivol_collection.py
@@ -96,12 +96,6 @@ raw_ts = raw_ts.dropna(how = 'all', axis = 1)
 atm_vols = raw_ts.dropna()
 
 
-"""
-# =============================================================================
-                                           cleaning the term structure manually
-"""
-
-
 strike_spread = raw_ts.iloc[:,0].dropna().index
 spot = float(np.median(strike_spread))
 
@@ -118,96 +112,3 @@ T = np.sort(spread_ts.columns)
 K = np.sort(spread_ts.index)
 s = np.median(K)
 
-
-from Derman import derman
-derman = derman()
-
-def compute_derman_coefs(T,K,ts_df):
-    derman_coefs = {}
-    for i, k in enumerate(K):
-        for j, t in enumerate(T):
-            b, alpha, atmvol, derman_ivols = derman.compute_derman_ivols(t,ts_df)
-            derman_coefs[t] = [b, alpha, atmvol]
-    derman_coefs = pd.DataFrame(derman_coefs)
-    derman_coefs['coef'] = ['b','alpha','atmvol']
-    derman_coefs.set_index('coef',inplace = True)
-    return derman_coefs
-
-derman_coefs = compute_derman_coefs(T,K,spread_ts)
-
-derman_maturities = np.sort(derman_coefs.columns)
-
-"""
-# =============================================================================
-                                    Derman reconstruction of volatility surface
-"""
-
-derman_ts_np = np.zeros((len(K),len(derman_maturities)),dtype=float)
-derman_ts = pd.DataFrame(derman_ts_np)
-derman_ts.index = K
-derman_ts.columns = derman_maturities
-
-
-
-for i, k in enumerate(K):
-    moneyness = k - s
-    for j, t in enumerate(derman_maturities):
-        derman_ts.iloc[i,j] = (
-            derman_coefs.loc['alpha',t] + atm_vols[t] + \
-                derman_coefs.loc['b',t] * moneyness
-                )
-
-from settings import model_settings
-ms = model_settings()
-derman_vol_matrix = ms.make_implied_vols_matrix(K, derman_maturities, derman_ts)
-
-print(derman_vol_matrix)
-
-expiration_dates = ms.compute_ql_maturity_dates(derman_maturities)
-
-derman_surface = ms.make_black_var_surface(
-    expiration_dates, K.astype(float), derman_vol_matrix)
-
-def plot_volatility_surface(black_var_surface = derman_surface,
-                            K = K,
-                            T = T,
-                            ts_df = spread_ts,
-                            target_maturity = 3):
-    import matplotlib.pyplot as plt
-    plt.rcParams['figure.figsize']=(15,7)
-    plt.style.use("dark_background")
-    from matplotlib import cm
-    target_maturity_ivols = ts_df[target_maturity]
-    fig, ax = plt.subplots()
-    ax.plot(K, target_maturity_ivols, label="Black Surface")
-    ax.plot(K, target_maturity_ivols, "o", label="Actual")
-    ax.set_xlabel("Strikes", size=9)
-    ax.set_ylabel("Vols", size=9)
-    ax.legend(loc="upper right")
-    fig.show()
-    
-    plot_maturities = np.sort(T/365).astype(float)
-    plot_strikes = np.sort(K).astype(float)
-    X, Y = np.meshgrid(plot_strikes, plot_maturities)
-    Z = np.array([[
-        black_var_surface.blackVol(y, x) for x in plot_strikes] 
-        for y in plot_maturities])
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    
-    surf = ax.plot_surface(X,Y,Z, rstride=1, cstride=1, cmap=cm.coolwarm,
-                    linewidth=0.1)
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    
-    ax.set_xlabel("Strikes", size=9)
-    ax.set_ylabel("Maturities (Years)", size=9)
-    ax.set_zlabel("Volatility", size=9)
-    
-    plt.show()
-    plt.cla()
-    plt.clf()
-    return fig
-
-
-fig = plot_volatility_surface()
