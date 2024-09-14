5ba7bbca22ac4b7ec28debe2a1c2abc0be5fa93f
radusbriciu <radusbriciu@proton.me>
2024-09-14 14:33:22 +0100

    robust generation
    implemented a generation technique that maps market data to unobserved combinations of contract_details free of assumptions

---
+++ module_Derman.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 13:30:47 2024
+
+@author: doomd
+"""
+
+module_derman()
--- save_files.py
+++ save_files.py
@@ -16,14 +16,15 @@ ticker = r"SPX"
 
 file_time = time.time()
 file_datetime = datetime.fromtimestamp(file_time)
-time_tag = file_datetime.strftime('%Y-%m-%d %H-%M-%S')
+# time_tag = file_datetime.strftime('%Y-%m-%d %H-%M-%S')
+time_tag = '2024-09-13'
 generic = f"{ticker} {time_tag}"
 
-from rountine_Derman import raw_ts, derman_ts, spread_ts, derman_coefs
-raw_ts.drop_duplicates().to_csv(f"{generic} raw_ts.csv")
-spread_ts.drop_duplicates().to_csv(f"{generic} spread_ts.csv")
-derman_ts.drop_duplicates().to_csv(f"{generic} derman_ts.csv")
-derman_coefs.drop_duplicates().to_csv(f"{generic} derman_coefs.csv")
+# from rountine_Derman import raw_ts, derman_ts, spread_ts, derman_coefs
+# raw_ts.drop_duplicates().to_csv(f"{generic} raw_ts.csv")
+# spread_ts.drop_duplicates().to_csv(f"{generic} spread_ts.csv")
+# derman_ts.drop_duplicates().to_csv(f"{generic} derman_ts.csv")
+# derman_coefs.drop_duplicates().to_csv(f"{generic} derman_coefs.csv")
 
 # from routine_collection import contract_details
 # contract_details.drop_duplicates().to_csv(f"{generic} contract_details.csv")
