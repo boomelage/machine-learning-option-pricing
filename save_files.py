f37b6243d9c91ced017efab9c07815a5111ff59a
radusbriciu <radusbriciu@proton.me>
2024-09-14 02:37:10 +0100

    qol

--- save_files.py
+++ save_files.py
@@ -5,6 +5,11 @@ Created on Sat Sep 14 02:04:00 2024
 
 @author: doomd
 """
+
+import os
+pwd = str(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(pwd)
+
 import time
 from datetime import datetime
 
@@ -18,6 +23,6 @@ time_tag = '2024-09-13'
 
 generic = f"{ticker} {time_tag}"
 raw_ts.to_csv(f"{generic} raw_ts.csv")
-spread_ts.to_csv(f"{generic} rspread_ts.csv")
+spread_ts.to_csv(f"{generic} spread_ts.csv")
 derman_ts.to_csv(f"{generic} derman_ts.csv")
 derman_coefs.to_csv(f"{generic} derman_coefs.csv")
