7b6b2283102e365b07501ebb7c500113f50d448a
radusbriciu <radusbriciu@proton.me>
2024-09-14 03:26:38 +0100

    qol

--- import_files.py
+++ import_files.py
@@ -12,19 +12,22 @@ import pandas as pd
 from data_query import dirdatacsv
 csvs = dirdatacsv()
 
-
 derman_coefs = pd.read_csv(csvs[0])
 derman_coefs = derman_coefs.set_index('coef')
 derman_coefs.columns = derman_coefs.columns.astype(int)
 
-derman_ts = pd.read_csv(csvs[1])
+derman_ts = pd.read_csv(csvs[2])
 derman_ts = derman_ts.rename(
     columns={derman_ts.columns[0]:'Strike'}).set_index('Strike')
 
-spread_ts = pd.read_csv(csvs[3])
+spread_ts = pd.read_csv(csvs[4])
 spread_ts = spread_ts.rename(
     columns={spread_ts.columns[0]:'Strike'}).set_index('Strike')
 
-raw_ts = pd.read_csv(csvs[2])
+raw_ts = pd.read_csv(csvs[3])
 raw_ts = raw_ts.rename(
     columns={raw_ts.columns[0]:'Strike'}).set_index('Strike')
+
+contract_details = pd.read_csv(csvs[1])
+contract_details = contract_details.drop(
+    columns = contract_details.columns[0])
--- save_files.py
+++ save_files.py
@@ -3,7 +3,6 @@
 """
 Created on Sat Sep 14 02:04:00 2024
 
-@author: doomd
 """
 
 import os
@@ -14,15 +13,18 @@ import time
 from datetime import datetime
 
 ticker = r"SPX"
-from rountine_Derman import raw_ts, derman_ts, spread_ts, K, T, s, derman_coefs
+# from rountine_Derman import raw_ts, derman_ts, spread_ts, derman_coefs
+from routine_collection import contract_details
 
 file_time = time.time()
 file_datetime = datetime.fromtimestamp(file_time)
 # time_tag = file_datetime.strftime('%Y-%m-%d %H-%M-%S')
 time_tag = '2024-09-13'
-
 generic = f"{ticker} {time_tag}"
-raw_ts.to_csv(f"{generic} raw_ts.csv")
-spread_ts.to_csv(f"{generic} spread_ts.csv")
-derman_ts.to_csv(f"{generic} derman_ts.csv")
-derman_coefs.to_csv(f"{generic} derman_coefs.csv")
+
+
+# raw_ts.to_csv(f"{generic} raw_ts.csv")
+# spread_ts.to_csv(f"{generic} spread_ts.csv")
+# derman_ts.to_csv(f"{generic} derman_ts.csv")
+# derman_coefs.to_csv(f"{generic} derman_coefs.csv")
+contract_details.to_csv(f"{generic} contract_details.csv")
