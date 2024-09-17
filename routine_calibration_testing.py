e6184c0c24017fe44c9802801c2d6f01a7be0fd9
radusbriciu <radusbriciu@proton.me>
2024-09-17 14:27:13 +0100

    experimenting with calibration methods

--- routine_calibration_testing.py
+++ routine_calibration_testing.py
@@ -11,10 +11,15 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('term_structure')
 sys.path.append('contract_details')
 sys.path.append('misc')
+import pandas as pd
+
+pd.set_option('display.max_columns',None)
 
 
 import numpy as np
-from routine_calibration import heston_parameters
+# from routine_calibration import heston_parameters
+
+from calibration import heston_parameters
 
 T = heston_parameters.index
 
@@ -48,8 +53,8 @@ def compute_bs_ivols(T):
     heston_parameters['ivol'] = ivol_col
     return heston_parameters
 
-
 heston_parameters = compute_bs_ivols(T)
-heston_parameters['vol_errror'] = heston_parameters['ivol']/heston_parameters['volatility']-1
+heston_parameters['vol_err'] = heston_parameters['ivol']/heston_parameters['volatility']-1
+heston_parameters = heston_parameters[~(abs(heston_parameters['vol_err'])>0.1)]
 
 print(heston_parameters)
