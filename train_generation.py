f5164cb6827a2f938efed11f2e9bbe0b2179c945
radusbriciu <radusbriciu@proton.me>
2024-09-19 18:16:58 +0100

    checkpoint
    entire routine functional for select data, ml tests pending

--- train_generation.py
+++ train_generation.py
@@ -70,7 +70,7 @@ S = [ms.s]
 features_dataset = pd.DataFrame()
 
 n_k = int(1e5) #ms.n_k
-print(f'generating {int(2*n_k*len(T))} contract_features')
+print(f'\n\ngenerating {int(2*n_k*len(T))} contract_features\n')
 
 import numpy as np
 call_K_train = np.linspace(min(call_K),min(call_K),n_k)
