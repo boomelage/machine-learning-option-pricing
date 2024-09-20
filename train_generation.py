8e424e3ce9fa8cf240b92241ab77195c93af8c49
radusbriciu <radusbriciu@proton.me>
2024-09-20 14:47:50 +0100

    slight correction of calibration error by cherrypicking maturities

--- train_generation.py
+++ train_generation.py
@@ -13,26 +13,21 @@ sys.path.append('contract_details')
 sys.path.append('misc')
 import pandas as pd
 from itertools import product
-from settings import model_settings
 
-ms = model_settings()
-s = ms.s
-ticker = ms.ticker
-day_count = ms.day_count
-calendar = ms.day_count
-calculation_date = ms.day_count
 
-def generate_features(K,T,s):
+def generate_features(K,T,s,flag):
     features = pd.DataFrame(
         product(
             [s],
             K,
             T,
+            flag
             ),
         columns=[
             "spot_price", 
             "strike_price",
             "days_to_maturity",
+            "w"
                   ])
     return features
 
@@ -60,43 +55,50 @@ def apply_derman_vols(row):
 
 
 
-T = ms.T
-call_K = ms.call_K[:4]
-put_K = ms.put_K[-4:]
 
-S = [ms.s]
 
-n_k = int(1e3) #ms.n_k
-pricing_spread = 0.05
+from settings import model_settings
+ms = model_settings()
 import numpy as np
+s = ms.s
+call_K = ms.call_K[:5]
+put_K = ms.put_K[-5:]
 
-call_K_train = np.linspace(
-    ms.s*(1-pricing_spread),
-    max(ms.calibration_call_K),
-    n_k
-    )
+n_KT = int(1e2/2)
 
-put_K_train = np.linspace(
-    min(ms.calibration_put_K),
-    ms.s*(1+pricing_spread),
-    n_k
-    )
+call_K_interp = np.linspace(min(call_K), max(call_K),n_KT)
+put_K_interp = np.linspace(min(put_K),max(put_K),n_KT)
 
-call_features = generate_features(call_K_train,T,s)
-call_features['w'] = 'call'
-call_features['moneyness'] = call_features['spot_price'] - call_features['strike_price']
+T = np.linspace(1,31,n_KT)
+
+call_features = generate_features(call_K_interp, T, s, ['call'])
+put_features = generate_features(put_K_interp, T, s, ['put'])
 
 
-put_features = generate_features(put_K_train,T,s)
-put_features['w'] = 'put'
-put_features['moneyness'] = put_features['strike_price'] - put_features['spot_price']
+features = pd.concat([call_features,put_features],ignore_index=True).reset_index(drop=True)
 
-features = pd.concat([put_features,call_features])
+def compute_moneyness_row(row):
+    s = row['spot_price']
+    k = row['strike_price']
+    
+    if row['w'] == 'call':
+        call_moneyness = s-k
+        row['moneyness'] = call_moneyness
+        return row
+    elif row['w'] == 'put':
+        put_moneyness = k-s
+        row['moneyness'] = put_moneyness
+        return row
+    else:
+        raise ValueError('\n\n\nflag error')
+
+features = features.apply(compute_moneyness_row,axis = 1)
+
+
+
 
 features['dividend_rate'] = 0.02
 features['risk_free_rate'] = 0.04
-
-
 
 
 from routine_calibration_global import heston_parameters
@@ -107,11 +109,12 @@ features['kappa'] = heston_parameters['kappa'].iloc[0]
 features['rho'] = heston_parameters['rho'].iloc[0]
 features['v0'] = heston_parameters['v0'].iloc[0]
 
-features = features.apply(apply_derman_vols,axis=1).reset_index(drop=True)
-features.describe()
+# features = features.apply(apply_derman_vols,axis=1).reset_index(drop=True)
+# features.describe()
 
 from bilinear_interpolation import bilinear_vol_row
-# features = features.apply(bilinear_vol_row,axis=1)
+features = features.apply(bilinear_vol_row,axis=1)
+
 
 from pricing import black_scholes_price, noisyfier, heston_price_vanilla_row
 bs_features = features.apply(black_scholes_price,axis=1)
@@ -133,3 +136,4 @@ print(f"\n\ndescriptive statistics:\n{ml_data.describe()}")
 pd.reset_option('display.max_columns')
 
 # pd.reset_option('display.max_rows')
+
