8704da35275919b4aeb23ac450ff77e070c9b039
radusbriciu <radusbriciu@proton.me>
2024-09-21 17:00:38 +0100

    historical impvol collection started
    major progress. historical data currently running with decent accuracy on daily data from 2007 to 2012 all days included

--- historical_data/routine_historical_collection.py
+++ historical_data/routine_historical_collection.py
@@ -11,17 +11,14 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 import pandas as pd
 import numpy as np
-from itertools import product
-from settings import model_settings
-ms = model_settings()
+from data_query import dirdatacsv
+csvs = dirdatacsv()
 
 """
 data collection and cleaning
 """
 
 os.chdir(current_dir)
-csvs = ms.csvs
-
 
 # pd.set_option("display.max_rows",None)
 pd.set_option("display.max_columns",None)
@@ -30,38 +27,39 @@ pd.reset_option("display.max_rows")
 # pd.reset_option("display.max_columns")
 
 
-historical_impvols = pd.DataFrame()
-
-for file in csvs:
-    raw = pd.read_csv(file)
-    raw.columns = raw.loc[2]
-    
-    df = raw.loc[4:].reset_index(drop=True)
-    
-    df = df.replace(0,np.nan).dropna(subset=['Mid Price'])
-    
-    df.columns
-    
-    df.rename(columns={
-        'SPX Index' : 'date', 
-        '30 Day IVOL at 100.0% Moneyness LIVE':'30D',
-        '60 Day IVOL at 100.0% Moneyness LIVE':"60D",
-        '3 Month 100% Moneyness Implied Volatility':"3M",
-        '6 Month IVOL at 100.0% Moneyness LIVE':"6M",
-        '1 Year 100% Moneyness Implied Volatility':"12M",
-        '18 Month IVOL at 100.0% Moneyness LIVE':"18M",
-        '24 Month IVOL at 100.0% Moneyness LIVE':"24M", 
-        'BEst Div Yld':"dividend_rate",
-        'End of Day Risk Free Rate Mid':"risk_free_rate", 
-        'Mid Price':"spot_price",
-        }, inplace=True)
-
-    df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y',errors='coerce')
-    df = df.infer_objects(copy=False) 
-    df[df.columns[1:]] = df[df.columns[1:]].astype(float)
-    df.iloc[:, 1:] = df.iloc[:, 1:].apply(pd.to_numeric, errors='coerce')
-    
-    historical_impvols = pd.concat([historical_impvols,df],ignore_index=True)
 
 
+def collect_historical_data():
+    historical_data = pd.DataFrame()
+    for file in csvs:
+        raw = pd.read_csv(file)
+        raw.columns = raw.loc[2]
+        
+        df = raw.loc[4:].reset_index(drop=True)
+        
+        df = df.replace(0,np.nan).dropna(subset=['Mid Price'])
+        
+        df.columns
+        
+        df.rename(columns={
+            'SPX Index' : 'date', 
+            '30 Day IVOL at 100.0% Moneyness LIVE':'30D',
+            '60 Day IVOL at 100.0% Moneyness LIVE':"60D",
+            '3 Month 100% Moneyness Implied Volatility':"3M",
+            '6 Month IVOL at 100.0% Moneyness LIVE':"6M",
+            '1 Year 100% Moneyness Implied Volatility':"12M",
+            '18 Month IVOL at 100.0% Moneyness LIVE':"18M",
+            '24 Month IVOL at 100.0% Moneyness LIVE':"24M", 
+            'BEst Div Yld':"dividend_rate",
+            'End of Day Risk Free Rate Mid':"risk_free_rate", 
+            'Mid Price':"spot_price",
+            }, inplace=True)
+    
+        df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y',errors='coerce')
+        df = df.infer_objects(copy=False) 
+        df[df.columns[1:]] = df[df.columns[1:]].astype(float)
+        df.iloc[:, 1:] = df.iloc[:, 1:].apply(pd.to_numeric, errors='coerce')
+        
+        historical_data = pd.concat([historical_data,df],ignore_index=True)
+    return historical_data
 
