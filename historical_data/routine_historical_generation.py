33c96f37b232e3d8a76564808846f9b4eb406a6a
boomelage <163606270+boomelage@users.noreply.github.com>
2024-09-21 17:00:38 +0100

    historical impvol collection started
    major progress. historical data currently running with decent accuracy on daily data from 2007 to 2012 all days included

--- historical_data/routine_historical_generation.py
+++ historical_data/routine_historical_generation.py
@@ -13,12 +13,15 @@ import pandas as pd
 import numpy as np
 import QuantLib as ql
 from itertools import product
-from settings import model_settings
-ms = model_settings()
-from routine_historical_collection import historical_impvols
 from bicubic_interpolation import bicubic_vol_row
 from routine_calibration_global import calibrate_heston
 from pricing import noisyfier
+from settings import model_settings
+ms = model_settings()
+os.chdir(current_dir)
+from routine_historical_collection import collect_historical_data
+
+
 # pd.set_option("display.max_rows",None)
 pd.set_option("display.max_columns",None)
 
@@ -59,80 +62,83 @@ def generate_train_features(K,T,s,flag):
 """
 historical generation routine
 """
+historical_data = collect_historical_data()
 
-historical_impvols
-    
-row = historical_impvols.iloc[0]
-s = row['spot_price']
-dtdate = row['date']
+historical_option_data = pd.DataFrame()
+for i, row in historical_data.iterrows():
+    try:
+        s = row['spot_price']
+        dtdate = row['date']
+        
+        calculation_date = ql.Date(dtdate.day,dtdate.month,dtdate.year)
+        
+        expiry_dates = np.array([
+               calculation_date + ql.Period(30,ql.Days), 
+                calculation_date + ql.Period(60,ql.Days), 
+                calculation_date + ql.Period(3,ql.Months), 
+                 calculation_date + ql.Period(6,ql.Months),
+               #  calculation_date + ql.Period(12,ql.Months), 
+               # calculation_date + ql.Period(18,ql.Months), 
+               # calculation_date + ql.Period(24,ql.Months)
+              ],dtype=object)
+        T = expiry_dates - calculation_date
+        
+        """
+        calibration
+        """
+        
+        call_K = np.linspace(s, s*1.01, 5)
+        put_K  =   np.arange(s*0.99, s, 5)
+        
+        calls = generate_features(call_K, T, s)
+        calls = calls[calls['days_to_maturity'].isin(T)].copy()
+        calls['w'] = 'call'
+        calls['moneyness'] = calls['spot_price'] - calls['strike_price']
+        
+        puts = generate_features(put_K, T, s)
+        puts = puts[puts['days_to_maturity'].isin(T)].copy()
+        puts['w'] = 'put'
+        puts['moneyness'] = puts['strike_price'] - puts['spot_price']
+        
+        calls = calls.apply(bicubic_vol_row,axis=1)
+        puts = puts.apply(bicubic_vol_row,axis=1)
+        
+        features = pd.concat([calls,puts],ignore_index=True)
+        features['dividend_rate'] = row['dividend_rate']
+        
+        """
+        calibration output (assuming fixed risk free rate for now)
+        """
+        
+        features['risk_free_rate'] = 0.04
+        heston_parameters = calibrate_heston(features,s)
+        
+        """
+        generation
+        """
+        
+        pricing_spread = 0.005
+        call_K_interp = np.linspace(s, s*(1+pricing_spread),1000)
+        put_K_interp = np.linspace(s*(1-pricing_spread),s,1000)
+        
+        call_features = generate_train_features(call_K_interp,T,s,['call'])
+        put_features = generate_train_features(put_K_interp,T,s,['put'])
+        
+        features = pd.concat(
+            [call_features,put_features],ignore_index=True).reset_index(drop=True)
+        
+        features['sigma'] = heston_parameters['sigma'].iloc[0]
+        features['theta'] = heston_parameters['theta'].iloc[0]
+        features['kappa'] = heston_parameters['kappa'].iloc[0]
+        features['rho'] = heston_parameters['rho'].iloc[0]
+        features['v0'] = heston_parameters['v0'].iloc[0]
+        features['risk_free_rate'] = 0.04
+        features['dividend_rate'] = row['dividend_rate']
+        heston_features = features.apply(ms.heston_price_vanilla_row,axis=1)
+        ml_data = noisyfier(heston_features)
+        historical_option_data = pd.concat([historical_option_data,ml_data])
+        print(dtdate)
+    except Exception:
+        print(f"\n{dtdate}\n")
+        pass
 
-calculation_date = ql.Date(dtdate.day,dtdate.month,dtdate.year)
-
-expiry_dates = np.array([
-       calculation_date + ql.Period(30,ql.Days), 
-        calculation_date + ql.Period(60,ql.Days), 
-        calculation_date + ql.Period(3,ql.Months), 
-         calculation_date + ql.Period(6,ql.Months),
-       #  calculation_date + ql.Period(12,ql.Months), 
-       # calculation_date + ql.Period(18,ql.Months), 
-       # calculation_date + ql.Period(24,ql.Months)
-      ],dtype=object)
-T = expiry_dates - calculation_date
-
-"""
-calibration
-"""
-
-call_K = np.linspace(s, s*1.01, 5)
-put_K  =   np.arange(s*0.99, s, 5)
-
-calls = generate_features(call_K, T, s)
-calls = calls[calls['days_to_maturity'].isin(T)].copy()
-calls['w'] = 'call'
-calls['moneyness'] = calls['spot_price'] - calls['strike_price']
-
-puts = generate_features(put_K, T, s)
-puts = puts[puts['days_to_maturity'].isin(T)].copy()
-puts['w'] = 'put'
-puts['moneyness'] = puts['strike_price'] - puts['spot_price']
-
-calls = calls.apply(bicubic_vol_row,axis=1)
-puts = puts.apply(bicubic_vol_row,axis=1)
-
-features = pd.concat([calls,puts],ignore_index=True)
-features['dividend_rate'] = row['dividend_rate']
-
-"""
-calibration output (assuming fixed risk free rate for now)
-"""
-
-features['risk_free_rate'] = 0.04
-heston_parameters = calibrate_heston(features,s)
-
-"""
-generation
-"""
-
-pricing_spread = 0.005
-call_K_interp = np.linspace(s, s*(1+pricing_spread),100)
-put_K_interp = np.linspace(s*(1-pricing_spread),s,100)
-
-call_features = generate_train_features(call_K_interp,T,s,['call'])
-put_features = generate_train_features(put_K_interp,T,s,['put'])
-
-features = pd.concat(
-    [call_features,put_features],ignore_index=True).reset_index(drop=True)
-
-features['sigma'] = heston_parameters['sigma'].iloc[0]
-features['theta'] = heston_parameters['theta'].iloc[0]
-features['kappa'] = heston_parameters['kappa'].iloc[0]
-features['rho'] = heston_parameters['rho'].iloc[0]
-features['v0'] = heston_parameters['v0'].iloc[0]
-features['risk_free_rate'] = 0.04
-features['dividend_rate'] = row['dividend_rate']
-heston_features = features.apply(ms.heston_price_vanilla_row,axis=1)
-ml_data = noisyfier(heston_features)
-
-
-
-ml_data
