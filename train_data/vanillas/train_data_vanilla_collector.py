23f73f1746c9ccf47c625bf985056e627ebcccdb
boomelage <163606270+boomelage@users.noreply.github.com>
2024-09-27 11:20:30 +0100

    further testing

--- train_data/vanillas/train_data_vanilla_collector.py
+++ train_data/vanillas/train_data_vanilla_collector.py
@@ -7,13 +7,14 @@ Created on Thu Sep 26 01:57:11 2024
 
 import os
 import sys
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 grandparent_dir = os.path.dirname(parent_dir)
 sys.path.append(parent_dir)
 sys.path.append(grandparent_dir)
-import numpy as np
-import pandas as pd
 from data_query import dirdatacsv
 from settings import model_settings, compute_moneyness
 os.chdir(current_dir)
@@ -23,22 +24,28 @@ csvs = dirdatacsv()
 
 print('\nloading data...\n')
 
+file_bar = tqdm(desc="file",total=len(csvs),unit='files',leave=True)
 training_data = pd.DataFrame()
 for file in csvs:
     train_subset = pd.read_csv(file)
     training_data = pd.concat([training_data,train_subset],ignore_index=True)
-    
+    file_bar.update(1)
+file_bar.close()
+
 
 training_data = training_data.drop(
     columns=training_data.columns[0]).drop_duplicates()
-
+initial_count = training_data.shape[0]
 """
 maturities filter
 """
 
 # training_data = training_data[
-#     (abs(training_data['days_to_maturity'])>=0)&
-#     (abs(training_data['days_to_maturity'])<=31)
+    
+#     (training_data['days_to_maturity']>=0)
+#     &
+#     (training_data['days_to_maturity']<=31)
+    
 #     ].reset_index(drop=True)
 
 
@@ -46,7 +53,7 @@ maturities filter
 type filter
 """
 
-training_data = training_data[training_data.loc[:,'w'] == 'put']
+# training_data = training_data[training_data.loc[:,'w'] == 'put']
 
 
 """"""
@@ -56,8 +63,8 @@ training_data = compute_moneyness(training_data)
 """
 moneyness filter
 """
-lower = -1
-upper = 1
+lower = -0.1
+upper = 0.1
 
 training_data = training_data[
     (training_data['moneyness'] >=  lower ) 
@@ -73,7 +80,7 @@ training_data = training_data[
     ]
 
 training_data = training_data.loc[
-    training_data['observed_price'] >= 0.005 * training_data['spot_price']
+    training_data['observed_price'] >= 0.01*training_data['spot_price']
     ]
 
 S = np.sort(training_data['spot_price'].unique())
@@ -83,4 +90,6 @@ W = np.sort(training_data['w'].unique())
 pd.set_option("display.max_columns",None)
 print(f"\n{training_data.describe()}\n")
 print(f"\nspot(s):\n{S}\n\nstrikes:\n{K}\n\nmaturities:\n{T}\n\ntypes:\n{W}\n")
+print(f"\ninitial count:\n{initial_count}")
+print(f"\ntotal prices:\n{training_data.shape[0]}")
 pd.reset_option("display.max_columns")
