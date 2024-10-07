23336a0b73e5e0ac5dd88a7ce38f117a3c72ed2a
radusbriciu <radusbriciu@proton.me>
2024-10-07 23:31:12 +0100

    testing

---
+++ historical_data/historical_alphaVantage_collection.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 19:28:49 2024
+
+@author: boomelage
+"""
+
+import requests
+import pandas as pd
+from model_settings import ms
+import numpy as np
+import QuantLib as ql
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from model_settings import ms
+date = '2009-04-01'
+key = '******'
+symbol = 'SPY'
+pd.set_option("display.max_columns",None)
+
+underlying_url = str(
+    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
+    f"{symbol}&outputsize=full&apikey={key}"
+    )
+spotr = requests.get(underlying_url)
+spots = pd.DataFrame(spotr.json()['Time Series (Daily)']).T
+spots = spots.astype(float)
+
+spots['mid'] = np.array(
+    (spots['3. low'].values + spots['2. high'].values)/2
+    )
+spots.index = pd.to_datetime(spots.index,format='%Y-%m-%d')
+
+options_url = str(
+    "https://www.alphavantage.co/query?function=HISTORICAL_OPTIONS&"
+    f"symbol={symbol}"
+    f"&date={date}"
+    f"&apikey={key}"
+          )
+
+r = requests.get(options_url)
+data = r.json()
+
+df = pd.DataFrame(data['data'])
+df = df.rename(
+    columns={
+        'expiration':'expiration_date',
+        'date':'calculation_date',
+        'strike':'strike_price',
+        'type':'w',
+        'implied_volatility':'volatility'
+        }
+    )
+
+df =  df[
+    [
+     'strike_price','volatility','w','bid','ask',
+     'calculation_date', 'expiration_date'
+     ]
+    ]
+
+df['calculation_date'] = pd.to_datetime(df['calculation_date'])
+df['expiration_date'] = pd.to_datetime(df['expiration_date'])
+columns_to_convert = ['strike_price', 'volatility', 'bid', 'ask']
+df[columns_to_convert] = df[
+    columns_to_convert].apply(pd.to_numeric, errors='coerce')
+df['mid'] = (df['bid'].values + df['ask'].values)/2
+
+
+df['spot_price'] = df['calculation_date'].map(spots['mid'])
+df['days_to_maturity'] = (
+    df['expiration_date'] - df['calculation_date']).dt.days
+df['moneyness'] = ms.vmoneyness(df['spot_price'], df['strike_price'], df['w'])
+puts = df[df['w']=='put'].copy().reset_index(drop=True)
+
+puts = puts[np.abs(puts['moneyness'])<0.3].copy()
+
+
+s = float(puts['spot_price'].unique()[0])
+T = np.sort(puts['days_to_maturity'].unique().astype(float)).tolist()
+K = np.sort(puts['strike_price'].unique().astype(float)).tolist()
+
+
+puts = puts.set_index(['days_to_maturity','strike_price'])
+
+
+ivol_df = pd.DataFrame(
+    np.zeros((len(K),len(T)),dtype=float),
+    index = K,
+    columns = T
+    )
+
+for k in K:
+    for t in T:
+        try:
+            ivol_df.loc[k,t] = puts.loc[(t,k),'volatility']
+        except Exception:
+            ivol_df.loc[k,t] = np.nan
+
+
+ivol_df = ivol_df.dropna(how='all',axis=1).dropna(how='all',axis=0)
+ivol_df = ivol_df.fillna(0.0)
+
+
+K = ivol_df.index.tolist()
+T = ivol_df.columns.tolist()
+
+ql_ivols = ql.Matrix(len(K),len(T),0.0)
+for i,k in enumerate(K):
+    for j,t in enumerate(T):
+        ql_ivols[i][j] = float(ivol_df.loc[k,t])
+
+
+bicubic_vol = ql.BicubicSpline(T, K, ql_ivols)
+
+
+print(bicubic_vol(t,k,False))
+print(ivol_df.loc[k,t])
+
+K = np.linspace(min(K),max(K),100)
+T = np.linspace(min(T),180,100)
+KK,TT = np.meshgrid(K,T)
+
+V = np.array(
+    [[bicubic_vol(float(t),float(k),False) for k in K] for t in T]
+    )
+
+
+plt.rcParams['figure.figsize']=(7,5)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.view_init(elev=20, azim=30)  
+surf = ax.plot_surface(KK,TT,V, rstride=1, cstride=1, cmap=cm.coolwarm,
+                linewidth=0.1)
+fig.colorbar(surf, shrink=0.3, aspect=5)
+
+ax.set_xlabel("Strikes", size=9)
+ax.set_ylabel("Maturities (Years)", size=9)
+ax.set_zlabel("Volatility", size=9)
+
+plt.tight_layout() 
+plt.show()
+plt.cla()
+plt.clf()
---
+++ historical_data/historical_generation/.ipynb_checkpoints/HDF_collection-checkpoint.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 09:38:02 2024
+
+"""
+import os
+import sys
+import time
+import pandas as pd
+from datetime import datetime
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+grandparent_dir = os.path.dirname(parent_dir)
+sys.path.append(parent_dir)
+sys.path.append(grandparent_dir)
+
+from model_settings import ms
+os.chdir(current_dir)
+
+print("\nimporting dataset(s)...\n")
+
+"""
+data selection
+"""
+
+
+barriers_dir = os.path.join(current_dir,'historical_barrier_generation')
+sys.path.append(barriers_dir)
+h5_file_path = os.path.join(barriers_dir,'SPX barriers.h5')
+
+
+# vaillas_dir = os.path.join(current_dir,'historical_vanilla_generation')
+# sys.path.append(vaillas_dir)
+# h5_file_path = os.path.join(vaillas_dir,'SPX vanillas.h5')
+
+
+""""""
+collection_start_time = time.time()
+with pd.HDFStore(h5_file_path, 'r') as hdf_store:
+    keys = hdf_store.keys()
+""""""
+  
+"""
+date filter
+"""
+start_date = datetime.strptime("2007-01-01", "%Y-%m-%d")
+end_date = datetime.strptime("2007-01-05", "%Y-%m-%d")
+keys = pd.Series(keys)
+date_pattern = r"(\d{4}_\d{2}_\d{2})"
+extracted_dates = keys.str.extract(date_pattern, expand=False)
+keys_dates = pd.to_datetime(
+    extracted_dates, format="%Y_%m_%d", errors='coerce')
+filtered_keys = keys[(keys_dates >= start_date) & (keys_dates <= end_date)]
+filtered_keys.tolist()
+
+
+
+"""
+loading selection
+"""
+LOADING_KEYS = keys
+
+
+bar = tqdm(desc='loading',total=len(LOADING_KEYS))
+contracts_list = []
+with pd.HDFStore(h5_file_path, 'r') as hdf_store: 
+    for key in LOADING_KEYS:
+        contracts_list.append(hdf_store.get(key))
+        bar.update(1)
+contracts = pd.concat(contracts_list, ignore_index=True)
+bar.close()
+contracts.dtypes
+
+
+print('\npreparing data...\n')
+if 'barrier_price' in contracts.columns:
+    contracts = contracts[contracts['barrier_price']>0]
+    contracts['observed_price'] = ms.noisyfier(contracts['barrier_price'])
+    
+elif 'heston_price' in contracts.columns:
+    contracts = contracts[contracts['heston_price']>0]
+    contracts['observed_price'] = ms.noisyfier(contracts['heston_price'])
+else:
+    raise ValueError('no price found in contracts dataset')
+
+contracts = contracts.reset_index(drop=True)
+
+pd.set_option("display.max_columns",None)
+print(f"\n{contracts.describe()}")
+print(f"\n{contracts.dtypes}\n")
+collection_end_time = time.time()
+collection_runtime = collection_end_time - collection_start_time
+print(f"\nruntime: {round(collection_runtime,4)} seconds\n")
+
+
+
+histogram_tolerance = 1
+hist_contracts = contracts[
+    'observed_price'][contracts['observed_price']>histogram_tolerance].copy()
+percent_exclusion = 1-len(hist_contracts)/contracts['observed_price'].shape[0]
+
+spots = contracts.set_index('calculation_date')['spot_price'].drop_duplicates()
+plt.figure()
+plt.plot(spots,color='black')
+plt.xticks(rotation=45)
+plt.title('SPX index time series considered')
+plt.ylabel('spot price')
+plt.yticks(rotation=45)
+plt.show()
+plt.clf()
+plt.hist(
+    hist_contracts,
+    bins=int(round(len(contracts['observed_price'].values)**0.5,0))
+    )
+plt.title('distribution of observed option '
+          f'prices greater than {histogram_tolerance}')
+plt.xlabel('option price')
+plt.ylabel(f'frequency ({round(percent_exclusion,2)}% of data excluded)')
+plt.yticks(rotation = 45)
+plt.show()
+plt.clf()
