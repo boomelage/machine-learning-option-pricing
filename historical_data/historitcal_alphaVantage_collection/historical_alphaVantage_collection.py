df5db9480c0f6503134dd2d0bdd972635111c288
boomelage <163606270+boomelage@users.noreply.github.com>
2024-10-10 01:43:43 +0100

    testing

--- historical_data/historitcal_alphaVantage_collection/historical_alphaVantage_collection.py
+++ historical_data/historitcal_alphaVantage_collection/historical_alphaVantage_collection.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  7 19:28:49 2024
-
-@author: boomelage
-"""
-
+from model_settings import ms
 import requests
-import pandas as pd
 import numpy as np
+from scipy import interpolate
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import QuantLib as ql
 from datetime import datetime
-import concurrent.futures
-pd.set_option("display.max_columns",None)
-import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(current_dir)
+
 
 def generate_daily_dates(start_date, end_date):
     date_range = pd.date_range(start=start_date, end=end_date, freq='D')
@@ -21,128 +15,110 @@ def generate_daily_dates(start_date, end_date):
     return date_strings
 
 
-def collect_option_chain_link(date, symbol, key):
-    option_chain_link = {}
-    underlying_url = str(
-        "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY"
-        f"&symbol={symbol}&date={date}&outputsize=full&apikey={key}"
-        )
-    spotr = requests.get(underlying_url)
-    spots = pd.DataFrame(spotr.json()['Time Series (Daily)']).T
-    spots = spots.astype(float)
-    
-    spots['mid'] = np.array(
-        (spots['3. low'].values + spots['2. high'].values)/2
-        )
-    spots.index = pd.to_datetime(spots.index,format='%Y-%m-%d')
-    
+key = ms.av_key
+symbol = 'SPY'
+end_date = '2024-05-17'
+dates = generate_daily_dates('2024-05-17', end_date)
+
+underlying_url = str(
+    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY"
+    f"&symbol={symbol}&date={end_date}&outputsize=full&apikey={key}"
+    )
+spotr = requests.get(underlying_url)
+spots = pd.DataFrame(spotr.json()['Time Series (Daily)']).T
+spots = spots.astype(float)
+
+spots.index = pd.to_datetime(spots.index,format='%Y-%m-%d')
+
+
+
+for date in dates:
     options_url = str(
         "https://www.alphavantage.co/query?function=HISTORICAL_OPTIONS&"
         f"symbol={symbol}"
         f"&date={date}"
         f"&apikey={key}"
               )
-    
+    spot = float(spots['4. close'][date])
+
     r = requests.get(options_url)
     data = r.json()
     
+    
     df = pd.DataFrame(data['data'])
-    df = df.rename(
-        columns={
-            'expiration':'expiration_date',
-            'date':'calculation_date',
-            'strike':'strike_price',
-            'type':'w',
-            'implied_volatility':'volatility'
-            }
-        )
-    
-    df =  df[
-        [
-         'strike_price','volatility','w','bid','ask',
-         'calculation_date', 'expiration_date'
-         ]
-        ]
-    
-    df['calculation_date'] = pd.to_datetime(df['calculation_date'])
-    df['expiration_date'] = pd.to_datetime(df['expiration_date'])
-    columns_to_convert = ['strike_price', 'volatility', 'bid', 'ask']
+    columns_to_convert = ['strike', 'last', 'mark',
+           'bid', 'bid_size', 'ask', 'ask_size', 'volume', 'open_interest',
+           'implied_volatility', 'delta', 'gamma', 'theta', 'vega', 'rho']
     df[columns_to_convert] = df[
         columns_to_convert].apply(pd.to_numeric, errors='coerce')
-    df['mid'] = (df['bid'].values + df['ask'].values)/2
-    df['spot_price'] = df['calculation_date'].map(spots['mid'])
-    df['days_to_maturity'] = (
-        df['expiration_date'] - df['calculation_date']).dt.days
     
-    calls = df[df['w']=='call'].copy().reset_index(drop=True)
-    T = np.sort(calls['days_to_maturity'].unique().astype(float)).tolist()
-    K = np.sort(calls['strike_price'].unique().astype(float)).tolist()
-    surf_calls = calls.set_index(['days_to_maturity','strike_price'])
-    call_surf = pd.DataFrame(
-        np.zeros((len(K),len(T)),dtype=float),
-        index = K,
-        columns = T
-        )
+    df['expiration'] = pd.to_datetime(df['expiration'],format='%Y-%m-%d')
+    df['date'] = pd.to_datetime(df['date'],format='%Y-%m-%d')
+    df['days_to_maturity'] = df['expiration'] - df['date']
+    df['days_to_maturity'] = df['days_to_maturity'] / np.timedelta64(1, 'D')
+    df['days_to_maturity'] = df['days_to_maturity'].astype('int64')
+    df = df[(df['days_to_maturity']>=30)&(df['days_to_maturity']<=400)]
+    
+    df = df[df['volume']>0].copy()
+    
+    indexed = df.copy().set_index(['strike','days_to_maturity'])
+    
+    T = np.sort(df['days_to_maturity'].unique()).tolist()
+    K = np.sort(df['strike'].unique()).tolist()
+    volume_heatmap = pd.DataFrame(
+        np.full((len(K), len(T)), np.nan), index=K, columns=T)
     for k in K:
         for t in T:
             try:
-                call_surf.loc[k,t] = surf_calls.loc[(t,k),'volatility']
+                volume_heatmap.loc[k,t] = indexed.loc[(k,t),'volume']
             except Exception:
-                call_surf.loc[k,t] = np.nan
-                
+                pass
     
-    puts = df[df['w']=='put'].copy().reset_index(drop=True)
-    T = np.sort(puts['days_to_maturity'].unique().astype(float)).tolist()
-    K = np.sort(puts['strike_price'].unique().astype(float)).tolist()
-    surf_puts = puts.set_index(['days_to_maturity','strike_price'])
-    put_surf = pd.DataFrame(
-        np.zeros((len(K),len(T)),dtype=float),
+    hottest_contracts = pd.DataFrame(volume_heatmap.unstack().sort_values(
+        ascending=False)).head(50).reset_index()
+    hottest_contracts.columns = ['t','k','volume']
+    T = np.sort(hottest_contracts['t'].unique()).tolist()
+    K = np.sort(hottest_contracts['k'].unique()).tolist()
+    
+    vol_matrix = pd.DataFrame(
+        np.full((len(K),len(T)),np.nan),
         index = K,
         columns = T
-        )
+    )
     for k in K:
         for t in T:
             try:
-                put_surf.loc[k,t] = surf_puts.loc[(t,k),'volatility']
+                vol_matrix.loc[k,t] = indexed.loc[
+                    (k,float(t)),'implied_volatility']
             except Exception:
-                put_surf.loc[k,t] = np.nan
+                pass
     
-    option_chain_link['call'] = {
-        'surface':call_surf,
-        'contracts': calls
-        }
+    vol_matrix = vol_matrix.dropna().copy()
+    T = vol_matrix.columns.tolist()
+    K = vol_matrix.index.tolist()
     
-    option_chain_link['put'] = {
-        'surface':put_surf,
-        'contracts':puts
-        }
+    print(vol_matrix)
+    # KK,TT = np.meshgrid(K,T)
+    # VV = np.array(
+    #         [[vol_matrix.loc[k,t] for k in K] for t in T]
+    #         )
+    # fig = plt.figure(figsize=plt.figaspect(0.5))
     
-    return option_chain_link
-
-def collect_chain(start_date, end_date, symbol, key):
-    dates = generate_daily_dates(start_date, end_date)
-    chain = {}
-    def fetch_data(date):
-        try:
-            link = collect_option_chain_link(date, symbol, key)
-            return date, link
-        except Exception:
-            calculation_datetime = datetime.strptime(
-                date, "%Y-%m-%d").strftime("%A, %Y-%m-%d")
-            print(f"no data for {calculation_datetime}")
-            return date, None
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = list(executor.map(fetch_data, dates))
-    chain = {date: data for date, data in results if data is not None}
-    return chain
-
-
-
-from model_settings import ms
-key = ms.av_key
-symbol = 'SPY'
-start_date = '2024-01-01'
-end_date = '2024-01-10'
-# end_date = datetime.today().strftime("%Y-%m-%d")
-chain = collect_chain(start_date, end_date, symbol, key)
+    # ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+    # surf = ax1.plot_surface(KK, TT, VV, rstride=1, cstride=1, cmap=cm.magma, linewidth=0.1)
+    
+    # ax1.set_zlim(0, float(max(vol_matrix.values.flatten())))
+    # ax1.set_title('volatiltiy surface')
+    # ax1.set_xlabel('strike')
+    # ax1.set_ylabel('maturity')
+    
+    # ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+    # ax2.plot_wireframe(KK, TT, VV, rstride=1, cstride=1,color='black')
+    # ax2.set_zlim(0, float(max(vol_matrix.values.flatten())))
+    # ax2.set_title('volatiltiy wireframe')
+    # ax2.set_xlabel('strike')
+    # ax2.set_ylabel('maturity')
+    
+    # plt.tight_layout()
+    # plt.show()
+    
