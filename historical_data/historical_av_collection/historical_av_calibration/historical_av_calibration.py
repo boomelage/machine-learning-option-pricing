fe69fc0904ddefb4d696c8258d3ef828a62b1231
radusbriciu <radusbriciu@proton.me>
2024-10-13 16:23:59 +0100

    testing

--- historical_data/historical_av_collection/historical_av_calibration/historical_av_calibration.py
+++ historical_data/historical_av_collection/historical_av_calibration/historical_av_calibration.py
@@ -5,11 +5,6 @@ from pathlib import Path
 from model_settings import ms
 import pandas as pd
 import numpy as np
-from model_settings import ms
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import matplotlib.dates as mdates
 from datetime import datetime
 from datetime import timedelta
 import QuantLib as ql
