1ddd2d85f0b549a06c7bf9e165927c16ad93955d
radusbriciu <radusbriciu@proton.me>
2024-10-13 11:08:39 +0100

    testing

--- historical_data/historical_av_collection/historical_av_underlying_fetcher.py
+++ historical_data/historical_av_collection/historical_av_underlying_fetcher.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from datetime import datetime
 from model_settings import ms
 from historical_av_key_collector import keys_df, symbol
 """
@@ -16,7 +17,13 @@ r = requests.get(url)
 spots = pd.Series(pd.DataFrame(r.json()['Time Series (Daily)']).transpose()['4. close'].squeeze())
 spots = pd.to_numeric(spots,errors='coerce')
 spots.index = pd.to_datetime(spots.index,format='%Y-%m-%d')
-spots = spots[~spots.index.isin(keys_df['date'])]
+spots = spots[
+	~(
+		(spots.index.isin(keys_df['date']))
+		&
+		(spots.index == datetime(2020,6,23))
+	)
+]
 spots.index = spots.index.strftime('%Y-%m-%d')
 print(spots)
 
