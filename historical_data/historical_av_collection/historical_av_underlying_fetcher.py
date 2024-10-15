e24b76d524bc1bc52d338f6e4e7c7b9139621fd3
radusbriciu <radusbriciu@proton.me>
2024-10-15 16:23:53 +0100

    testing

--- historical_data/historical_av_collection/historical_av_underlying_fetcher.py
+++ historical_data/historical_av_collection/historical_av_underlying_fetcher.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 from model_settings import ms
-from historical_av_key_collector import keys_df, symbol, available_dates
+from historical_av_key_collector import keys_df, symbol, dates
 """
 https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=demo
 """
@@ -17,8 +17,7 @@ spots = pd.to_numeric(spots,errors='coerce').reset_index().rename(columns={'inde
 spots = spots.set_index('date')
 historical_spots = spots.copy()
 try:
-	spots = spots[~spots.index.isin(available_dates)]
-	spots = spots[spots.index != '2020-06-23']
+	spots = spots[~spots.index.isin(dates)]
 except Exception as e:
 	pass
 print(f"\ndata to collect for {symbol}:\n{spots}")
