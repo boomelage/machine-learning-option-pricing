37d25574ab606c8f693c117e5526f5fc6a6812a1
boomelage <163606270+boomelage@users.noreply.github.com>
2024-10-22 01:22:28 +0100

    testing

--- asian_option_generation/asian_option_generation.py
+++ asian_option_generation/asian_option_generation.py
@@ -1,280 +1,125 @@
 import os
-import time
 import numpy as np
 import pandas as pd
 from itertools import product
-import matplotlib.pyplot as plt 
 from joblib import Parallel, delayed
 from datetime import datetime
-from pathlib import Path
-import QuantLib as ql
 
-class asian_option_pricer():
-    def __init__(self,seed=123):
-        if seed == None:
-            self.seed = 0
-        else:
-            self.seed = seed
-        self.rng = "pseudorandom" # could use "lowdiscrepancy"
-        self.numPaths = 100000
-        print('Asian option pricer initialized')
-        print('day counter is QuantLib 30/360 USA\n')
+class asian_option_feature_generation():
+    def generate_asian_option_features(self,s,r,g,calculation_datetime,kappa,theta,rho,eta,v0):
+        kupper = int(s*(1+0.5))
+        klower = int(s*(1-0.5))
+        K = np.arange(klower,kupper,5)
 
-    def asian_option_price(self,s,k,r,g,w,averaging_type,n_fixings,fixing_frequency,past_fixings,kappa,theta,rho,eta,v0,calculation_datetime):
-        s = float(s)
-        k = float(k)
-        r = float(r)
-        g = float(g)
+        W = [
+            'call',
+            'put'
+        ]
 
-        
-        if w == 'call':
-            option_type = ql.Option.Call 
-        elif w == 'put':
-            option_type = ql.Option.Put
-        t = n_fixings*fixing_frequency
-        
-
-        calculation_date = ql.Date(calculation_datetime.day,calculation_datetime.month,calculation_datetime.year)
-        
-        periods = np.arange(fixing_frequency,t+1,fixing_frequency).astype(int)
-        fixing_periods = [ql.Period(int(p),ql.Days) for p in periods]
-        fixing_dates = [calculation_date + p for p in fixing_periods]
-
-        expiration_date = calculation_date + fixing_periods[-1]
+        types = [
+            'arithmetic',
+            'geometric'
+        ]
 
 
-        riskFreeTS = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date,float(r),ql.Thirty360(ql.Thirty360.USA)))
-        dividendTS = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date,float(g),ql.Thirty360(ql.Thirty360.USA)))
+        past_fixings = [0]
 
-        hestonProcess = ql.HestonProcess(riskFreeTS, dividendTS, ql.QuoteHandle(ql.SimpleQuote(s)), v0, kappa, theta, eta, rho)
-        hestonModel = ql.HestonModel(hestonProcess)
-        vanillaPayoff = ql.PlainVanillaPayoff(option_type, float(k))
-        europeanExercise = ql.EuropeanExercise(expiration_date)
-        
-        if averaging_type == 'geometric':
-            geometric_engine = ql.MCDiscreteGeometricAPHestonEngine(hestonProcess, self.rng, requiredSamples=self.numPaths,seed=self.seed)
-            geometricAverage = ql.Average().Geometric
-            geometricRunningAccumulator = 1.0
-            discreteGeometricAsianOption = ql.DiscreteAveragingAsianOption(
-                geometricAverage, geometricRunningAccumulator, past_fixings,
-                fixing_dates, vanillaPayoff, europeanExercise)
-            discreteGeometricAsianOption.setPricingEngine(geometric_engine)
-            geometric_price = float(discreteGeometricAsianOption.NPV())
-            return geometric_price
-            
-        else:
-            arithmetic_engine = ql.MCDiscreteArithmeticAPHestonEngine(hestonProcess, self.rng, requiredSamples=self.numPaths)
-            arithmeticAverage = ql.Average().Arithmetic
-            arithmeticRunningAccumulator = 0.0
-            discreteArithmeticAsianOption = ql.DiscreteAveragingAsianOption(
-                arithmeticAverage, arithmeticRunningAccumulator, past_fixings, 
-                fixing_dates, vanillaPayoff, europeanExercise)
-            discreteArithmeticAsianOption.setPricingEngine(arithmetic_engine)
-            arithmetic_price = float(discreteArithmeticAsianOption.NPV())
-            return arithmetic_price
+        fixing_frequencies = [
+            1,7,28,84,168,336,672
+        ]
 
-    def row_asian_option_price(self,row):
-        return  self.asian_option_price(
+        maturities = [
+            7,28,84,168,336,672
+        ]
+        features = []
+        for i,t in enumerate(maturities):
+            for f in fixing_frequencies[:i+1]:
+                n_fixings = [t/f]
+                df = pd.DataFrame(
+                    product(
+                        [s],
+                        K,
+                        [t],
+                        n_fixings,
+                        [f],
+                        [0],
+                        ['geometric','arithmetic'],
+                        ['call','put'],
+                        [r],
+                        [g],
+                        [calculation_datetime],
+                        [kappa],
+                        [theta],
+                        [rho],
+                        [eta],
+                        [v0]
+                    ),
+                    columns = [
+                        'spot_price','strike_price','days_to_maturity',
+                        'n_fixings','fixing_frequency','past_fixings','averaging_type','w',
+                        'risk_free_rate','dividend_rate','calculation_date',
+                        'kappa','theta','rho','eta','v0'
+                    ]
+                )
+                features.append(df)
+
+        for f in fixing_frequencies:
+            features.append(
+                pd.DataFrame(
+                    product(
+                        [s],
+                        K,
+                        [f],
+                        [1],
+                        [f],
+                        [0],
+                        ['geometric','arithmetic'],
+                        ['call','put'],
+                        [r],
+                        [g],
+                        [calculation_datetime],
+                        [kappa],
+                        [theta],
+                        [rho],
+                        [eta],
+                        [v0]
+                    ),
+                    columns = [
+                        'spot_price','strike_price','days_to_maturity',
+                        'n_fixings','fixing_frequency','past_fixings','averaging_type','w',
+                        'risk_free_rate','dividend_rate','calculation_date',
+                        'kappa','theta','rho','eta','v0'
+                    ]
+                )
+            )
+        return pd.concat(features,ignore_index=True)
+
+    def row_generate_asian_option_features(self,row):
+        return self.generate_asian_option_features(
             row['spot_price'],
-            row['strike_price'],
             row['risk_free_rate'],
             row['dividend_rate'],
-            row['w'],
-            row['averaging_type'],
-            row['fixing_frequency'],
-            row['n_fixings'],
-            row['past_fixings'],
+            row['date'],
             row['kappa'],
             row['theta'],
             row['rho'],
             row['eta'],
-            row['v0'],
-            row['calculation_date']
+            row['v0']
         )
 
-    def df_asian_option_price(self, df):
-        max_jobs = os.cpu_count() // 4
 
-        max_jobs = max(1, max_jobs)
-
-        return Parallel(n_jobs=max_jobs)(delayed(self.row_asian_option_price)(row) for _, row in df.iterrows())
-
-def generate_asian_options(s,r,g,calculation_datetime,kappa,theta,rho,eta,v0):
-    kupper = int(s*(1+0.5))
-    klower = int(s*(1-0.5))
-    K = np.arange(klower,kupper,7)
-
-    W = [
-        'call',
-        'put'
-    ]
-
-    types = [
-        # 'arithmetic',
-        'geometric'
-    ]
-
-    fixing_frequencies = [
-        # 1,
-        # 7,
-        30,
-        # 90,
-    ]
-
-    n_fixings = [
-        1,
-        # 3,
-        # 6,12
-    ]
-
-    past_fixings = [0]
+    def df_generate_asian_option_features(self,df):
+        return Parallel()(delayed(self.row_generate_asian_option_features)(row) for _, row in df.iterrows())
 
 
-    features = pd.DataFrame(
-        product(
-            [s],
-            K,
-            [r],
-            [g],
-            W,
-            types,
-            fixing_frequencies,
-            n_fixings,
-            past_fixings,
-            [kappa],
-            [theta],
-            [rho],
-            [eta],
-            [v0],
-            [calculation_datetime]
-        ),
-        columns = [
-            'spot_price','strike_price','risk_free_rate','dividend_rate','w',
-            'averaging_type','fixing_frequency','n_fixings','past_fixings',
-            'kappa','theta','rho','eta','v0','calculation_date'
-        ]
-    )
-    features['days_to_maturity'] = features['n_fixings']*features['fixing_frequency']
-    features['asian_price'] = aop.df_asian_option_price(features)
-
-    datetag = calculation_datetime.strftime('%Y_%m_%d')
-    filetag = datetime.today().strftime('%Y-%m-%d %H%M%S')
-    filename = f"{datetag} asian options {filetag}.csv"
-    filepath = os.path.join(str(Path().resolve()),'historical_asian_options',filename)
-    features.to_csv(filepath)
-
-def row_generate_asian_options(row):
-    generate_asian_options(
-        row['spot_price'],
-        row['risk_free_rate'],
-        row['dividend_rate'],
-        row['date'],
-        row['kappa'],
-        row['theta'],
-        row['rho'],
-        row['eta'],
-        row['v0']
-    )
-
-
-def df_generate_asian_options(df):
-    Parallel()(delayed(row_generate_asian_options)(row) for _, row in df.iterrows())
-
-
-
-
-"""
-
-usage
-
-
-"""
-
-aop = asian_option_pricer()
-
-
-
-
-calibrations = pd.read_csv([file for file in os.listdir(str(Path().resolve())) if file.find('SPY calibrated')!=-1][0]).iloc[:1,1:]
+from pathlib import Path
+calibrations = pd.read_csv([file for file in os.listdir(str(Path().resolve())) if file.find('av_recalibrated')!=-1][0]).iloc[:,1:]
+calibrations = calibrations.rename(columns={'calculation_date':'date'})
 calibrations['date'] = pd.to_datetime(calibrations['date'],format='%Y-%m-%d')
+calibrations['risk_free_rate'] = 0.04
 
-df_generate_asian_options(calibrations)
+gen = asian_option_feature_generation()
 
-
-# print(calibrations.dtypes)
-# calibrations['risk_free_rate'] = 0.04
-
-# day = {}
-# for i in calibrations.columns:
-#     day[i] = calibrations[i].iloc[0]
-#     print(f"{i} {calibrations[i].iloc[0]}")
-# s = day['spot_price']
-# g = day['dividend_rate']
-# calculation_datetime = day['date']
-# theta = day['theta']
-# kappa = day['kappa']
-# rho = day['rho']
-# eta = day['eta']
-# v0 = day['v0']
-# r = day['risk_free_rate']
-
-
-# kupper = int(s*(1+0.5))
-# klower = int(s*(1-0.5))
-# K = np.arange(klower,kupper,7)
-
-# W = [
-#     'call',
-#     'put'
-# ]
-
-# types = [
-#     # 'arithmetic',
-#     'geometric'
-# ]
-
-# fixing_frequencies = [
-#     # 1,
-#     # 7,
-#     30,
-#     # 90,
-# ]
-
-# n_fixings = [
-#     1,
-#     # 3,
-#     # 6,12
-# ]
-
-# past_fixings = [0]
-
-
-# features = pd.DataFrame(
-#     product(
-#         [s],
-#         K,
-#         [r],
-#         [g],
-#         W,
-#         types,
-#         fixing_frequencies,
-#         n_fixings,
-#         past_fixings,
-#         [kappa],
-#         [theta],
-#         [rho],
-#         [eta],
-#         [v0],
-#         [calculation_datetime]
-#     ),
-#     columns = [
-#         'spot_price','strike_price','risk_free_rate','dividend_rate','w',
-#         'averaging_type','fixing_frequency','n_fixings','past_fixings',
-#         'kappa','theta','rho','eta','v0','calculation_date'
-#     ]
-# )
-
-# # features['asian_price'] = aop.df_asian_option_price(features)
-
-# print(features)
-
+features = gen.df_generate_asian_option_features(calibrations)
+features = pd.concat(features,ignore_index=True)
+print(features)
