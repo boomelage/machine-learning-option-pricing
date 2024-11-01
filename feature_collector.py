862c573c33ea3eb95fc976081a941974cc375930
radusbriciu <radusbriciu@proton.me>
2024-11-01 17:25:11 +0000

    testing

--- feature_collector.py
+++ feature_collector.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 def collect_features(datadir,price_name):
@@ -23,3 +24,30 @@ def collect_features(datadir,price_name):
 	dataset['calculation_date'] = pd.to_datetime(dataset['calculation_date'])
 	dataset = dataset.sort_values(by='calculation_date',ascending=False).reset_index(drop=True)
 	return dataset
+
+
+def preprocess_data(dataset,development_dates,test_dates,trainer):
+    train_data = dataset[dataset['calculation_date'].isin(development_dates)]
+    test_data = dataset[dataset['calculation_date'].isin(test_dates)]
+    
+    train_plot = train_data[['calculation_date','spot_price']].copy().set_index('calculation_date').drop_duplicates()
+    test_plot = test_data[['calculation_date','spot_price']].copy().set_index('calculation_date').drop_duplicates()
+    
+    trainplotx = pd.date_range(start=min(train_plot.index),end=max(train_plot.index),periods=len(train_plot))
+    testplotx = pd.date_range(start=min(test_plot.index),end=max(test_plot.index),periods=len(test_plot))
+    
+    plt.figure()
+    plt.xticks(rotation=45)
+    plt.plot(testplotx,test_plot,color='purple',label='out-of-sample')
+    plt.plot(trainplotx,train_plot,color='green',label='in-sample')
+    plt.legend()
+    plt.show()
+    arrs = trainer.get_train_test_arrays(
+        train_data, test_data)
+    train_X = arrs['train_X']
+    train_y = arrs['train_y']
+    test_X = arrs['test_X']
+    test_y = arrs['test_y']
+    preprocessor = trainer.preprocess()
+    print(len(train_y),len(train_X))
+    return {'preprocessor':preprocessor,'train_test_arrays':arrs}
