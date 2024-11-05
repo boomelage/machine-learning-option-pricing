963a9d7753c40ae781f671fe929a68a66c8a1e3d
radusbriciu <radusbriciu@proton.me>
2024-11-05 01:21:37 +0000

    testing

--- feature_collector.py
+++ feature_collector.py
@@ -3,33 +3,6 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-import warnings; warnings.filterwarnings("ignore", message=".*defaulting*")
-
-
-def collect_features(datadir,price_name):
-    files = [f for f in os.listdir(datadir) if f.endswith('.csv')]
-    files = [os.path.join(datadir,f) for f in files]
-    dfs = []
-    bar = tqdm(total=len(files)+1)
-    for f in files:
-        dfs.append(pd.read_csv(f).iloc[:,1:])
-        bar.update(1)
-    dataset = pd.concat(dfs,ignore_index=True).dropna().reset_index(drop=True)
-    bar.update(1)
-    bar.close()
-    print()
-    dataset['calculation_date'] = pd.to_datetime(dataset['calculation_date'],format='mixed')
-    try:
-        dataset['date'] = pd.to_datetime(dataset['date'],format='mixed')
-    except Exception:
-        pass
-    dataset[price_name] = pd.to_numeric(dataset[price_name],errors='coerce')
-    dataset = dataset[dataset[price_name]<=dataset['spot_price']]
-    dataset['observed_price'] = np.maximum(dataset[price_name] + np.random.normal(scale=(0.15)**2,size=dataset.shape[0]),0)
-    dataset = dataset[dataset['observed_price']>=0.01]
-    dataset = dataset.sort_values(by='calculation_date',ascending=False).reset_index(drop=True)
-    return dataset
-
 
 def preprocess_data(dataset,development_dates,test_dates,trainer):
     try:
