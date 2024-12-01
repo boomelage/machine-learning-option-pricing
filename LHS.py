94a62c1812ea19316f3fcbe30b4f0723c8893807
boomelage <163606270+boomelage@users.noreply.github.com>
2024-12-01 13:02:27 +0000

    testing

--- LHS.py
+++ LHS.py
@@ -2,7 +2,7 @@ from scipy.stats import qmc
 import pandas as pd
 pd.set_option('display.max_columns',None)
 sampler = qmc.LatinHypercube(d=5)
-sample = sampler.random(n=100000)
+sample = sampler.random(n=int(1e5))
 
 
 l_bounds = [
@@ -28,6 +28,22 @@ sample_scaled = pd.DataFrame(
 	columns=['kappa','theta','rho','eta','v0']
 )
 
-
-
+sample_scaled['feller'] = 2*sample_scaled['kappa']*sample_scaled['theta']-sample_scaled['eta']**2
+sample_scaled = sample_scaled[abs(sample_scaled)<1].dropna().reset_index(drop=True)
 print(sample_scaled.describe())
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.pairplot(sample_scaled)
+plt.show()
+
+from pathlib import Path
+from model_settings import ms
+ms.find_root(Path())
+ms.collect_spx_calibrations()
+market = ms.spx_calibrations[sample_scaled.columns]
+# market = market[abs(market)<1].dropna().reset_index(drop=True)
+
+print(market.describe())
+sns.pairplot(market)
+plt.show()
