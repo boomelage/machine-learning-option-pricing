54f55c6f3e441e18dac4f729ad6358da8ca03fb8
boomelage <163606270+boomelage@users.noreply.github.com>
2025-09-01 21:42:50 +0100

    Delete calculated directory

--- calculated/calibrated/av/SPY_av_calibrations/av_SPY.py
+++
@@ -1,7 +0,0 @@
-import os
-from pathlib import Path
-from heston_model_calibration import calibrations
-prev = Path().resolve().absolute()
-os.chdir(Path(__file__).parent)
-calibrations = calibrations.collect_calibrations()
-os.chdir(prev)
